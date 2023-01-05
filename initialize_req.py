d831cf4b2db67e994c3c5f7a619934503fae600c
Atharva Shetty <94366989+Atharva-Shetty@users.noreply.github.com>
2023-01-05 16:39:18 +0530

    Update initialize_req.py

--- initialize_req.py
+++ initialize_req.py
@@ -2,5 +2,5 @@ import os
 for f in os.listdir('./static/images'):
     new_name = f.replace(' ' , '')
     if f != new_name:
-        os.rename('./static/images/' + f, './static2/images/' + new_name)
+        os.rename('./static/images/' + f, './static/images/' + new_name)
  
